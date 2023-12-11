8c4478ef4ce17d1ba3a0eb9be48ad66452300a70
Philip Johnson <philipjohnson961021@gmail.com>
2023-12-11 09:48:00 -0600

    fixed mobile responsive of saved books page.

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -20,9 +20,7 @@ const SavedBooks = () => {
 
   const userData = data?.me || {};
 
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
-    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -34,7 +32,6 @@ const SavedBooks = () => {
         variables: { bookId },
       });
 
-      // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
       console.error(err);
@@ -44,89 +41,59 @@ const SavedBooks = () => {
   if (loading) {
     return <h2>LOADING...</h2>;
   }
-  const truncateText = (text, maxLength) => {
-    if (text && text.length > maxLength) {
-      return `${text.slice(0, maxLength)}...`;
-    }
-    return text;
-  };
+
   return (
     <>
       <Jumbotron fluid className="text-light bg-dark">
         <Container>
-          <h1>Viewing {userData.username}'s books!</h1>
+          <h1 className="text-center">Viewing {userData.username}'s books!</h1>
         </Container>
       </Jumbotron>
       <Container>
-        <h2>
+        <h2 className="text-center">
           {userData.savedBooks?.length
             ? `Viewing ${userData.savedBooks.length} saved ${
                 userData.savedBooks.length === 1 ? "book" : "books"
               }:`
             : "You have no saved books!"}
         </h2>
-        <div
-          style={{
-            flexDirection: "col", // Set the direction to row
-            overflowX: "auto",
-          }}
-        >
+        <CardColumns>
           {userData &&
             userData.savedBooks?.map((book) => (
-              <div
-                key={book.bookId}
-                style={{
-                  minWidth: "250px",
-                  marginBottom: "10px",
-                  border: "1px solid #ccc",
-                  borderRadius: "8px",
-                  padding: "10px",
-                  boxSizing: "border-box",
-                  display: "flex",
-                  backgroundColor: "white",
-                }}
-              >
+              <Card key={book.bookId}>
                 {book.image && (
-                  <img
+                  <Card.Img
                     src={book.image}
                     alt={`The cover for ${book.title}`}
-                    style={{
-                      height: "auto",
-                      objectFit: "cover",
-                      marginBottom: "10px",
-                    }}
                   />
                 )}
-                <div
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                    paddingLeft: "20px",
-                  }}
-                >
-                  <h5>{book.title}</h5>
-                  <p className="small">Authors: {book.authors}</p>
-                  <p>{truncateText(book.description, 250)}</p>
-                  <div
-                    style={{
-                      width: "500px",
-                    }}
+                <Card.Body>
+                  <Card.Title>{book.title}</Card.Title>
+                  <Card.Text className="small">
+                    Authors: {book.authors}
+                  </Card.Text>
+                  <Card.Text>{truncateText(book.description, 250)}</Card.Text>
+                  <Button
+                    variant="danger"
+                    block
+                    onClick={() => handleDeleteBook(book.bookId)}
                   >
-                    <Button
-                      className="btn-block btn-danger"
-                      onClick={() => handleDeleteBook(book.bookId)}
-                    >
-                      Delete this Book!
-                    </Button>
-                  </div>
-                </div>
-              </div>
+                    Delete this Book!
+                  </Button>
+                </Card.Body>
+              </Card>
             ))}
-        </div>
+        </CardColumns>
       </Container>
     </>
   );
 };
 
 export default SavedBooks;
+
+const truncateText = (text, maxLength) => {
+  if (text && text.length > maxLength) {
+    return `${text.slice(0, maxLength)}...`;
+  }
+  return text;
+};
